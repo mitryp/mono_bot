86110adb08141edc557bbdf07e3073728ef85dbe
Dmytro Popov <thedmitryp@ukr.net>
2024-10-05 19:53:58 +0100

    Tweaked webhook transaction representations
    Added representations for transactions in currencies not matching the account

--- mono_bot/application/services/presentation_service.py
+++ mono_bot/application/services/presentation_service.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Iterable, Final
 
 from mono_bot.domain.dtos.account_dto import AccountDto
-from mono_bot.domain.dtos.webhook_dto import WebhookDto
+from mono_bot.domain.dtos.webhook_dto import WebhookDto, StatementItemDto
 from mono_bot.domain.interfaces.config_service import IConfigService
 from mono_bot.domain.interfaces.presentation_service import IPresentationService
 from mono_bot.domain.models.account_declaration import AccountDeclaration
@@ -26,10 +26,26 @@ class PresentationService(IPresentationService):
 
     def represent_webhook(self, webhook: WebhookDto, account: AccountDto) -> str:
         item = webhook.statement_item
+        transaction_repr = self._represent_matching_transaction(item) \
+            if item.currency_code == account.currency_code \
+            else self._represent_not_matching_transaction(item, account)
 
-        return (f'**{item.float_amount} {self.represent_currency(item.currency_code)}**: '
+        return (f'{transaction_repr}: '
                 f'{item.description}\n'
                 f'Balance: {item.float_balance} {self.represent_currency(account.currency_code)}')
+
+    def _represent_matching_transaction(self, item: StatementItemDto) -> str:
+        """Represents a transaction with currency matching the account currency."""
+        return f'**{item.float_operation_amount} {self.represent_currency(item.currency_code)}**'
+
+    def _represent_not_matching_transaction(self, item: StatementItemDto, account: AccountDto) -> str:
+        """
+        Represents a transaction with currency not matching the account currency.
+
+        Will display the amount in the account currency near the operation amount.
+        """
+        return (f'{self._represent_matching_transaction(item)} '
+                f'({item.float_amount} {self.represent_currency(account.currency_code)})')
 
     def represent_currency(self, currency_code: int) -> str:
         repr_ = _CODE_TO_CURRENCY.get(currency_code, None)
